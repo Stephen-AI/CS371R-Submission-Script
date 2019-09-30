e1cf5d4f0c97b303eb212dc1bc2fb00af6b23096
Stephen Aigbomian <stephenaigbomian@gmail.com>
2019-09-30 14:18:12 -0500

    added another shell for running

--- zip_shell.sh
+++ zip_shell.sh
@@ -4,7 +4,8 @@ PROJNUM=""
 CLASSLIST=""
 MAINCLASS=""
 CORPORA=""
-while getopts e:n:l:m:c: o
+TRACENAME=""
+while getopts e:n:l:m:c:t: o
 do
     case $o in
         e) EID="$OPTARG";;
@@ -12,14 +13,15 @@ do
         l) CLASSLIST="$OPTARG";;
         m) MAINCLASS="$OPTARG";;
         c) CORPORA="$OPTARG";;
+        t) TRACENAME="$OPTARG";;
     esac
 done
 shift $OPTIND-1
 python3 submit.py -eid $EID -projnum $PROJNUM -classlist $CLASSLIST
-trace_file=$(echo "proj${PROJNUM}_${EID}_trace.txt" | tr -d "\r")
+trace_file=$(echo "proj${PROJNUM}_${EID}_${TRACENAME}_trace.txt" | tr -d "\r")
 echo $trace_file
 main_class=$(find ir -name "${MAINCLASS}")
 IFS='.' # hyphen (-) is set as delimiter
 read -ra ADDR <<< "$main_class"
 class="${ADDR[0]}"
-script -c "java ${class} -html ${CORPORA}" "${trace_file}"
+script -c "java ${class} -html -proximity ${CORPORA}" "${trace_file}"
