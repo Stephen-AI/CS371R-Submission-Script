0a4f90eec1f5e941f69f77c799052b15344bf302
Stephen Aigbomian <stephenaigbomian@gmail.com>
2019-11-02 14:14:33 -0500

    Script for project 3. More efficient now that it doesnt compile on everytime zip_shell is run

--- run_shell.sh
+++ run_shell.sh
@@ -1,14 +1,17 @@
 EID="sa46979"
 PROJNUM="3"
 PREFIX="proj${PROJNUM}_${EID}"
-RETRIEVE_TRACE="${PREFIX}_retrive_trace.txt"
+RETRIEVE_TRACE="${PREFIX}_retrieve_trace.txt"
 TEMP_TRACE="${PREFIX}_temp_trace.txt"
 
-./zip_shell.sh -e $EID -m PageRankSiteSpider.java -n $PROJNUM -l sources.txt -c 200 -d crawled -t spider -u http://www.cs.utexas.edu/~mooney/ir-course/proj3/a.html 
+python3 submit.py -eid $EID -projnum $PROJNUM -classlist sources.txt
+./zip_shell.sh -p $PREFIX -m PageRankSiteSpider.java -c 200 -d crawled -t spider -u http://www.cs.utexas.edu/~mooney/ir-course/proj3/a.html 
+rm $RETRIEVE_TRACE
 touch $RETRIEVE_TRACE
-./zip_shell.sh -e $EID -m PageRankInvertedIndex.java -n $PROJNUM -l sources.txt -t temp -d crawled -w 0.0
+./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 0.0 -r crawled
 cat $TEMP_TRACE >> $RETRIEVE_TRACE
-./zip_shell.sh -e $EID -m PageRankInvertedIndex.java -n $PROJNUM -l sources.txt -t temp -d crawled -w 5.0
+./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 5.0 -r crawled
 cat $TEMP_TRACE >> $RETRIEVE_TRACE
-./zip_shell.sh -e $EID -m PageRankInvertedIndex.java -n $PROJNUM -l sources.txt -t temp -d crawled -w 10.0
+./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 10.0 -r crawled
 cat $TEMP_TRACE >> $RETRIEVE_TRACE
+rm $TEMP_TRACE
--- zip_shell.sh
+++ zip_shell.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-EID=''
-PROJNUM=""
-CLASSLIST=""
+prefix=""
 MAINCLASS=""
 CORPORA=""
 TRACENAME=""
@@ -10,24 +8,20 @@ C=""
 D=""
 WEIGHT=""
 CORPORA=""
-while getopts e:n:l:m:u:t:c:d:w:r:* o
+while getopts p:m:u:t:c:d:w:r:* o
 do
     case $o in
-        e) EID="$OPTARG";;
-        n) PROJNUM="$OPTARG";;
-        l) CLASSLIST="$OPTARG";;
+        p) prefix="$OPTARG";;
         m) MAINCLASS="$OPTARG";;
         t) TRACENAME="$OPTARG";;
         u) U="-u $OPTARG";;
         c) C="-c $OPTARG";;
         d) D="-d $OPTARG";;
-        w) WEIGHT="-w $OPTARG";;
+        w) WEIGHT="-weight $OPTARG";;
         r) CORPORA="$OPTARG";;
     esac
 done
 shift $OPTIND-1
-python3 submit.py -eid $EID -projnum $PROJNUM -classlist $CLASSLIST
-prefix="proj${PROJNUM}_${EID}"
 trace_file="${prefix}_${TRACENAME}_trace.txt"
 main_class=$(find ir -name "${MAINCLASS}")
 IFS='.' # hyphen (-) is set as delimiter
