c3716fbd9579acacc9824c495d293cd5263a3f8e
Stephen Aigbomian <stephenaigbomian@gmail.com>
2019-11-10 10:05:49 -0600

    Made the compilation process more efficient by doing the compilation once in run_shell

--- run_shell.sh
+++ run_shell.sh
@@ -3,15 +3,16 @@ PROJNUM="3"
 PREFIX="proj${PROJNUM}_${EID}"
 RETRIEVE_TRACE="${PREFIX}_retrieve_trace.txt"
 TEMP_TRACE="${PREFIX}_temp_trace.txt"
+CRAWL_DIR="results"
 
 python3 submit.py -eid $EID -projnum $PROJNUM -classlist sources.txt
-./zip_shell.sh -p $PREFIX -m PageRankSiteSpider.java -c 200 -d crawled -t spider -u http://www.cs.utexas.edu/~mooney/ir-course/proj3/a.html 
+./zip_shell.sh -p $PREFIX -m PageRankSiteSpider.java -c 200 -d $CRAWL_DIR -t spider -u http://www.cs.utexas.edu/users/mooney/ir-course/favorite_classes.html
 rm $RETRIEVE_TRACE
 touch $RETRIEVE_TRACE
-./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 0.0 -r crawled
+./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 0.0 -r $CRAWL_DIR
 cat $TEMP_TRACE >> $RETRIEVE_TRACE
-./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 5.0 -r crawled
+./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 5.0 -r $CRAWL_DIR
 cat $TEMP_TRACE >> $RETRIEVE_TRACE
-./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 10.0 -r crawled
+./zip_shell.sh -p $PREFIX -m PageRankInvertedIndex.java -t temp -w 10.0 -r $CRAWL_DIR
 cat $TEMP_TRACE >> $RETRIEVE_TRACE
 rm $TEMP_TRACE
